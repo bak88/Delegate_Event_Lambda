9af9e311f79afa8d870ebde47b765a11fb22221b
bak88 <dimbak1990@gmail.com>
2024-09-24 13:33:22 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Delegate_Event_Lambda
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
