e9f083d8e2232e325603982ecfff8467061cf32c
bak88 <dimbak1990@gmail.com>
2024-10-27 21:10:13 +0300

    Excluded .vs folder from tracking

---
+++ test_DataBase/DataBase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace test_DataBase
+{
+    internal class DataBase
+    {
+        SqlConnection sqlConnection = new SqlConnection(@"Data Source=DESKTOP-6B2PDIN; Initial Catalog=test; Integrated Security=True");
+
+        public void openConnection() 
+        {
+            if (sqlConnection.State == System.Data.ConnectionState.Closed) // если коннект закрыт -> открыть
+            {
+                sqlConnection.Open();
+            }
+        }
+        public void closeConnection() 
+        {
+            if (sqlConnection.State == System.Data.ConnectionState.Open) 
+            {
+                sqlConnection.Close();
+            }
+        }
+
+        public SqlConnection getSqlConnection()
+        {
+            return sqlConnection;
+        }
+    }
+}
---
+++ test_DataBase/log_in.cs
@@ -0,0 +1,41 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test_DataBase
+{
+    public partial class log_in : Form
+    {
+        DataBase DataBase = new DataBase();
+
+        public log_in()
+        {
+            InitializeComponent();
+            StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        private void log_in_Load(object sender, EventArgs e)
+        {
+            textBox_password.PasswordChar = '*';
+            pictureBox3.Visible = false;
+            textBox_login.MaxLength = 50;
+            textBox_password.MaxLength = 50;
+        }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // log_in
+            // 
+            this.ClientSize = new System.Drawing.Size(315, 261);
+            this.Name = "log_in";
+            this.ResumeLayout(false);
+
+        }
+    }
+}
