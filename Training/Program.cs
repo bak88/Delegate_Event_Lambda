9ed49a381b81833e86bdd16d0a7886c054e42de8
bak88 <dimbak1990@gmail.com>
2024-11-18 21:18:18 +0300

    Creat Models

--- Training/Program.cs
+++ Training/Program.cs
@@ -10,32 +10,12 @@ namespace Training
     {
         public static void Main(string[] args)
         {
-            int x, y;
-            string direction, result = "";
 
-            string[] inputValues = ReadInput();
-            x = int.Parse(inputValues[0]);
-            y = int.Parse(inputValues[1]);
-            direction = inputValues[2];
+            double a =1.5
+                ; double b =2;
+            double c = a % b;
+            Console.WriteLine(c);
 
-
-            Console.WriteLine(result);
-        }
-
-        public static string[] ReadInput()
-        {
-            string[] inputValues = new string[3];
-
-            if (Console.ReadLine() is string input)
-            {
-                string[] values = input.Split(' ');
-                for (int i = 0; i < 3; i++)
-                {
-                    inputValues[i] = values[i];
-                }
-            }
-
-            return inputValues;
         }
 
 
